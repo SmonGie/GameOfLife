d8f4d7292d2077b5bafed03c80188d7e8c8f305d
SmonGie <sadfreeze@gmail.com>
2024-10-05 20:33:44 +0200

    just a commit with random main(not a big deal)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,19 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        double a = 20.00;
+        double b = 80.00;
+        double finalScore = (a + b)*100;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double moduloScore = finalScore % 40;
+        boolean isIt = (moduloScore == 0.00) ? true : false;
+
+        System.out.println("isIt: " + isIt);
+
+        if(!isIt)
+        {
+            System.out.println("got some remainder");
         }
+
     }
 }
