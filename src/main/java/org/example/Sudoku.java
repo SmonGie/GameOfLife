4c435740daccccf4fd2a71ce4c54606922f4d2d4
SmonGie <sadfreeze@gmail.com>
2024-10-05 21:03:20 +0200

    Preparing project for classes

--- src/main/java/org/example/Sudoku.java
+++ src/main/java/org/example/Sudoku.java
@@ -2,19 +2,18 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Sudoku {
     public static void main(String[] args) {
         double a = 20.00;
         double b = 80.00;
-        double finalScore = (a + b)*100;
+        double finalScore = (a + b) * 100;
 
         double moduloScore = finalScore % 40;
         boolean isIt = (moduloScore == 0.00) ? true : false;
 
         System.out.println("isIt: " + isIt);
 
-        if(!isIt)
-        {
+        if (!isIt) {
             System.out.println("got some remainder");
         }
 
