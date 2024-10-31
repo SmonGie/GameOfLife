33686daefae690591d9f442fc088431d1858820e
Szymon Giergiel 247663 <sadfreeze@gmail.com>
2024-10-31 17:00:30 +0100

    Zmiane przed rozpoczęciem kolejnego etapu

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,16 +3,25 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
     @Test
     public void mainTest() {
-        Main main = new Main();
         String simulatedInput = "5\n5\n3\n";
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
         Main.main(new String[]{});
-        assertTrue(true);
+
+        System.setOut(originalOut);
+        String output = outputStream.toString();
+        assertTrue(output.contains("Generation"));
     }
 }
